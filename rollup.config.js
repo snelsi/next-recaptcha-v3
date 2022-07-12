a9c8ac45270f4c4bfc227c53aab0cb1470203945
Roman Zhuravlov <snelsithepirate@gmail.com>
2022-07-12 13:29:51 +0300

    Migrate to rollup

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+
+const packageJson = require("./package.json");
+
+const config = {
+  input: "src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    terser(),
+  ],
+};
+
+export default config;
