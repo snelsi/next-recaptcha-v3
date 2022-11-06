728ac537fd82d69fa9e4ab7f7136723168a5197a
Roman Zhuravlov <snelsithepirate@gmail.com>
2022-11-06 23:18:44 +0200

    Upgrade to Rollup v3

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,32 +1,29 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 
-const packageJson = require("./package.json");
+import pkg from "./package.json" assert { type: "json" };
 
+/**
+ * @type {import('rollup').RollupOptions}
+ */
 const config = {
   input: "src/index.ts",
   output: [
     {
-      file: packageJson.main,
+      file: pkg.main,
       format: "cjs",
       sourcemap: true,
     },
     {
-      file: packageJson.module,
+      file: pkg.module,
       format: "esm",
       sourcemap: true,
     },
   ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    terser(),
-  ],
+  plugins: [peerDepsExternal(), commonjs(), resolve(), typescript(), terser()],
 };
 
 export default config;
