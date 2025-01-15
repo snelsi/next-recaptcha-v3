b56793563722b40da0b905bea2309ed55a3900e9
Roman Zhuravlov <snelsithepirate@gmail.com>
2025-01-15 05:34:55 +0200

    Update Eslint + dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: ["**/lib/**"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  prettierConfig,
+);
